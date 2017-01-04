1aa887f057b333514b395776a84367fb23df5872
ParadoxZero <sidhin.thomas@gmail.com>
2017-01-04 12:04:01 +0530

    Initial commit

---
+++ src/collector/main/Main.java
@@ -0,0 +1,11 @@
+package collector.main;
+
+/**
+ * Author: Sidhin S Thomas (ParadoxZero)
+ * Email : sidhin.thomas@gmail.com
+ */
+public class Main {
+    public static void main(String []args){
+        System.out.println("Starting data gathering...");
+    }
+}
