82e91399df1920722894dfba30aea9ca94e92d29
ParadoxZero <sidhin.thomas@gmail.com>
2017-01-11 13:58:26 +0530

    Initialization working successfully.

--- src/collector/main/Main.java
+++ src/collector/main/Main.java
@@ -1,11 +1,25 @@
 package collector.main;
 
+import collector.main.database.Database;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  * Author: Sidhin S Thomas (ParadoxZero)
  * Email : sidhin.thomas@gmail.com
  */
 public class Main {
     public static void main(String []args){
-        System.out.println("Starting data gathering...");
+        System.out.println("Initializing.....");
+        try {
+            Database db = new Database("jdbc:mysql://localhost/","root","root");
+        } catch (SQLException e) {
+            System.exit(10);
+        } catch (IOException e) {
+            System.exit(11);
+        } catch (ClassNotFoundException e) {
+            System.exit(12);
+        }
     }
 }
